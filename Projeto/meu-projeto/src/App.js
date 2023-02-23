aa3aa6cbaa19aa02d2100b0f7adbbe31a0ba6a88
ezequiel.ne@fatec.sp.gov.br <ezequiel.ne@fatec.sp.gov.br>
2023-02-22 23:22:25 -0300

    Adding Project/meu-projeto

---
+++ Projeto/meu-projeto/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const name = 'Ezequiel';
+  const newName = name.toUpperCase();
+  function sum(a,b){
+    return a+b;
+  }
+  const url = "http://via.placeholder.com/150";
+  return (
+    <div className="App">
+      <h2>Alterando o JSX</h2>
+      <p>Olá, {name}</p>
+      <p>Olá, {newName}</p>
+      <p>Soma: {2 + 2}</p>
+      <p>Soma: {sum(1,2)}</p>
+      <img src={url} alt="Minha Imagem"/>
+    </div>
+  );
+}
+
+export default App;
