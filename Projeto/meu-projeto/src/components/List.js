55e7997559aa9cddf8e38e0e6246906c652d5eca
ezequiel.ne@fatec.sp.gov.br <ezequiel.ne@fatec.sp.gov.br>
2023-03-06 22:05:42 -0300

    Creating the Item fragment and using it in the List

--- Projeto/meu-projeto/src/components/List.js
+++ Projeto/meu-projeto/src/components/List.js
@@ -1,10 +1,12 @@
+import Item from "./Item";
 function List(){
     return (
         <>
             <h1>Minha Lista</h1>
             <ul>
-                <li>Item 1</li>
-                <li>Item 2</li>
+                <Item marca="ferrari"/>
+                <Item marca="fiat"/>
+                <Item marca="renault"/>
             </ul>
         </>
     )
